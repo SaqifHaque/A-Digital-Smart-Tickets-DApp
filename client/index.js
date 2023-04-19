6b6d4fb7551ae9e609de373bb299992446b4028d
Saqif <saqifhaque@gmail.com>
2023-04-19 16:49:30 +0600

    Bootstrap and web3 module installed and configured

---
+++ client/index.js
@@ -0,0 +1,22 @@
+import Web3 from 'web3';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import configuration from '../build/contracts/Tickets.json';
+
+const CONTRACT_ADDRESS = configuration.networks['5777'].address;
+const CONTRACT_ABI = configuration.abi;
+
+const web3 = new Web3(
+    Web3.givenProvider || 'http://127.0.0.1:7545'
+);
+
+const contract = new  web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+let account;
+
+const accountEl = document.getElementById('account');
+
+const main = async () => {
+    const accounts = await web3.eth.requestAccounts();
+    account = accounts[0];
+    accountEl.innerText = account;
+}
